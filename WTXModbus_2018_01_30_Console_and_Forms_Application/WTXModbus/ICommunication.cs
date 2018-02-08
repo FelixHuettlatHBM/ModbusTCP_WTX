ef2ccc5b2ae8dfde43d5ac972c6404bf031ffe57
Felix Huettl \ HBM <Felix.Huettl@hbm.com>
2018-02-08 11:37:49 +0100

    no message

---
+++ WTXModbus_2018_01_30_Console_and_Forms_Application/WTXModbus/ICommunication.cs
@@ -0,0 +1,39 @@
+﻿/* @@@@ HOTTINGER BALDWIN MESSTECHNIK - DARMSTADT @@@@@
+ * 
+ * TCP/MODBUS Interface for WTX120 | 01/2018
+ * 
+ * Author : Felix Huettl 
+ * 
+ *  */
+
+
+namespace Hbm.Devices.WTXModbus
+{
+
+    /// <summary>
+    /// This interface contains the basic methods to realize a communication between a device and its master.
+    /// 
+    /// Method "Connect()" should establish a connection to the device and method "Is_connected()" should return 
+    /// whether there is a successful connection to the device or not.
+    /// 
+    /// The method ReadRegister() should read the register from the device. 
+    /// The method WriteRegister(data) should write data to the register. 
+    /// The method Close() should close the connection to the device.
+    /// 
+    /// There are several more ways to implement this, therefore another interface "Communication_Device_Interface" 
+    /// is derived from this interface to define the methods more precise. 
+    /// 
+    /// </summary>
+    interface ICommunication
+    {
+        void Connect();
+
+        bool is_connected{ get;}
+
+        void ReadRegister();
+
+        void WriteRegister(ushort data);
+
+        void Close();
+    }
+}
---
+++ WTXModbus_Demo_Console_and_Forms_Application/WTXModbus.bat
@@ -0,0 +1,1 @@
+call "WTXModbusCL.exe" 172.19.103.2 250 6

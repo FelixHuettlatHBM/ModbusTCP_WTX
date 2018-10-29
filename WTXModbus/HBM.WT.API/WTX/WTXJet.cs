0706a387ac86cff7db1922d7b46461f095b84474
Felix Huettl \ HBM <Felix.Huettl@hbm.com>
2018-10-29 12:28:43 +0100

    -WTXJet : Correction of 'handshake' via JetBus while setting to zero(='measure zero')

--- WTXModbus/HBM.WT.API/WTX/WTXJet.cs
+++ WTXModbus/HBM.WT.API/WTX/WTXJet.cs
@@ -504,14 +504,11 @@ namespace HBM.WT.API.WTX
             _connection.Write(ID_keys.SCALE_COMMAND, command_values.CALIBRATE_ZERO);       // SCALE_COMMAND = "6002/01"
 
             // check : command "on go" = command is in execution = 
-            while (_connection.Read(ID_keys.SCALE_COMMAND_STATUS) == 1634168417);
-
-            Thread.Sleep(1000);
-
+            while (_connection.Read(ID_keys.SCALE_COMMAND_STATUS) != 1634168417);
+            
             // check : command "ok" = command is done = 
-            while (_connection.Read(ID_keys.SCALE_COMMAND_STATUS) == 1801543519);
-
-            Thread.Sleep(1000);
+            while (_connection.Read(ID_keys.SCALE_COMMAND_STATUS) != 1801543519);
+            
         }
 
         public override void zeroing(Action<IDeviceData> WriteDataCompleted)
